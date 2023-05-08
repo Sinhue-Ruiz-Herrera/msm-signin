bf6e293f3680e8ccccb081541982bf77364a66cd
R. Sinhue Ruiz Herrera <renesinhue@gmail.com>
2023-05-08 16:34:49 +0000

    models completed with columns updated

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,10 +4,19 @@
 #
 #  id          :integer          not null, primary key
 #  description :text
+#  duration    :integer
+#  image       :string
 #  title       :string
+#  year        :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  belongs_to :director
+  has_many :actors
+
+  has_many :cast, :through => :characters, :source => :actor
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_151720) do
+ActiveRecord::Schema.define(version: 2023_05_08_163026) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2023_05_08_151720) do
     t.integer "director_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.integer "year"
+    t.integer "duration"
   end
 
 end
